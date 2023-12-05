1344f228f67d953bf6bff665c997a0b88b05ffce
Romain SAMSON <romain.samson@efrei.net>
2023-12-05 16:31:06 +0100

    add review css

--- src/components/GameDetails.vue
+++ src/components/GameDetails.vue
@@ -13,31 +13,34 @@
         @edit-game="editGame"
       />
   
-      <h3>Reviews</h3>
+      <h2>Reviews</h2>
       <ul>
         <li v-for="review in reviews" :key="review.review_id" class="review-item">
           <div class="review-content">
             <p>by {{ review.author }}<br>Grade: {{ review.grade }} /100<br>Review: {{ review.description }}</p>
+            <button @click="deleteReview(review.review_id)">Delete Review</button>
           </div>
         </li>
       </ul>
-  
-      <div class="center"><h3 >Add Review</h3></div>
+      <div class="center">
+      <h3 >Add Review</h3>
       <form @submit.prevent="submitReview">
-        <label for="description">Description:</label>
-        <textarea v-model="newReview.description" required></textarea>
-  
+        <div class="form-group">
+        <label for="author">Author:</label>
+        <input v-model="newReview.author" type="text" required><br>
+      </div>
+        <div class="form-group">
         <label for="grade">Grade:</label>
         <input v-model="newReview.grade" type="number" min="0" max="100" required>
-  
-        <label for="author">Author:</label>
-        <input v-model="newReview.author" type="text" required>
-  
         
-  
+      </div><br>
+        <div class="form-group">
+        <label for="description">Description:</label>
+        <textarea v-model="newReview.description" required></textarea>
+      </div>
         <button type="submit">Submit Review</button>
       </form>
-  
+    </div>
       <button @click="goBack">Back to Games</button>
     </div>
   </template>
@@ -93,6 +96,16 @@
                     console.error("Erreur lors de la soumission de la revue", error);
                 });
         },
+      deleteReview(reviewId) {
+        const gameId = this.$route.params.id;
+        axios.delete(`http://localhost:3000/games/review/${gameId}/${reviewId}`)
+            .then(() => {
+                this.getGameDetails();
+            })
+            .catch((error) => {
+                console.error("Erreur lors de la suppression de la revue", error);
+            });
+      },
       goBack() {
         this.$router.push({ path: '/' });
       },
@@ -102,32 +115,47 @@
   
 <style scoped>
 
-p,h3 {
+p,h3,h2 {
     color: var(--text);
     font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
         sans-serif;
     text-align: justify;
     padding: 20px ;
-    color: #fbfbfb;
+    
 }
 .review-item {
   list-style: none;
   border: 1px solid #ddd; 
   border-radius: 10px;
   width: 30%;
-  
+  margin-bottom: 15px;
   background-color: rgb(255, 251, 251)000; 
 }
 
 form {
     position:absolute;
     padding : 20px;
-    margin: 20px;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    margin: 40px;
+    left: 40%;
+    
     background-color: aliceblue;
     border-radius: 20px;
 }
-  
+h2,h3{
+  text-align: center;
+  font-size: 2em;
+}
+
+
+.form-group {
+  margin-bottom: 15px; 
+}
+
+label {
+  display: block; 
+  margin-bottom: 5px; 
+}
+
+
   </style>
   
