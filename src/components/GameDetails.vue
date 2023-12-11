0d5807e4b9aa0dec31b83ab0620a30f7efe5f9e0
Arthur Gatignol <36805161+arthur-gtgn@users.noreply.github.com>
2023-12-11 15:39:33 +0100

    Update styles and buttons in GameCard and HomeComponent

--- src/components/GameDetails.vue
+++ src/components/GameDetails.vue
@@ -1,86 +1,118 @@
 <template>
     <div class="game-details">
-      <SiteTopBar />
-      
-      <GameCard
-        :title="game.game_name"
-        :category="game.category"
-        :release_date="game.release_date ? game.release_date.substring(0, 10) : ''"
-        :description="game.description"
-        :rating="game.age_rating"
-        :gameId="game.game_id"
-        @delete-game="deleteGame"
-        @edit-game="editGame"
-      />
-  
-      <h2>Reviews</h2>
-      <ul>
-        <li v-for="review in reviews" :key="review.review_id" class="review-item">
-          <div class="review-content">
-            <p>by {{ review.author }}<br>Grade: {{ review.grade }} /100<br>Review: {{ review.description }}</p>
-            <button @click="deleteReview(review.review_id)">Delete Review</button>
-          </div>
-        </li>
-      </ul>
-      <div class="center">
-      <h3 >Add Review</h3>
-      <form @submit.prevent="submitReview">
-        <div class="form-group">
-        <label for="author">Author:</label>
-        <input v-model="newReview.author" type="text" required><br>
-      </div>
-        <div class="form-group">
-        <label for="grade">Grade:</label>
-        <input v-model="newReview.grade" type="number" min="0" max="100" required>
-        
-      </div><br>
-        <div class="form-group">
-        <label for="description">Description:</label>
-        <textarea v-model="newReview.description" required></textarea>
-      </div>
-        <button type="submit">Submit Review</button>
-      </form>
+        <SiteTopBar />
+        <div class="gamecard-container">
+            <GameCard
+                :title="game.game_name"
+                :category="game.category"
+                :release_date="
+                    game.release_date ? game.release_date.substring(0, 10) : ' '
+                "
+                :description="game.description"
+                :rating="game.age_rating"
+                :gameId="game.game_id"
+                @delete-game="deleteGame"
+                @edit-game="editGame"
+                class="gamecard"
+            />
+        </div>
+
+        <h2>Reviews</h2>
+        <ul>
+            <li
+                v-for="review in reviews"
+                :key="review.review_id"
+                class="review-item"
+            >
+                <div class="review-content">
+                    <p>
+                        by {{ review.author }}<br />Grade:
+                        {{ review.grade }} /100<br />Review:
+                        {{ review.description }}
+                    </p>
+                    <button @click="deleteReview(review.review_id)">
+                        Delete Review
+                    </button>
+                </div>
+            </li>
+        </ul>
+        <div class="center">
+            <h3>Add Review</h3>
+            <form @submit.prevent="submitReview">
+                <div class="form-group">
+                    <label for="author">Author:</label>
+                    <input
+                        v-model="newReview.author"
+                        type="text"
+                        required
+                    /><br />
+                </div>
+                <div class="form-group">
+                    <label for="grade">Grade:</label>
+                    <input
+                        v-model="newReview.grade"
+                        type="number"
+                        min="0"
+                        max="100"
+                        required
+                    />
+                </div>
+                <br />
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea
+                        v-model="newReview.description"
+                        required
+                    ></textarea>
+                </div>
+                <button type="submit">Submit Review</button>
+            </form>
+        </div>
+        <button @click="goBack">Back to Games</button>
     </div>
-      <button @click="goBack">Back to Games</button>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import SiteTopBar from "@/components/TopBarComponents/SiteTopBar.vue";
-  import GameCard from "@/components/SingleItemComponents/GameCard.vue";
-  
-  export default {
+</template>
+
+<script>
+import axios from "axios";
+import SiteTopBar from "@/components/TopBarComponents/SiteTopBar.vue";
+import GameCard from "@/components/SingleItemComponents/GameCard.vue";
+
+export default {
     components: {
-      SiteTopBar,
-      GameCard,
+        SiteTopBar,
+        GameCard,
     },
     data() {
-      return {
-        game: {},
-        reviews: [],
-        newReview: {
-          description: "",
-          grade: null,
-          author: "",
-          
-        },
-      };
+        return {
+            game: {},
+            reviews: [],
+            newReview: {
+                description: "",
+                grade: null,
+                author: "",
+            },
+        };
     },
     mounted() {
-      this.getGameDetails();
+        this.getGameDetails();
     },
     methods: {
-      getGameDetails() {
-        const gameId = this.$route.params.id;
-        axios.get(`http://localhost:3000/games/review/${gameId}`).then((response) => {
-          this.game = response.data.game;
-          this.reviews = response.data.reviews;
-        });
-      },
-      submitReview() {
+        getGameDetails() {
             const gameId = this.$route.params.id;
-            axios.post(`http://localhost:3000/games/review/${gameId}`, this.newReview)
+            axios
+                .get(`http://localhost:3000/games/review/${gameId}`)
+                .then((response) => {
+                    this.game = response.data.game;
+                    this.reviews = response.data.reviews;
+                });
+        },
+        submitReview() {
+            const gameId = this.$route.params.id;
+            axios
+                .post(
+                    `http://localhost:3000/games/review/${gameId}`,
+                    this.newReview
+                )
                 .then(() => {
                     // Rechargez les détails du jeu, y compris les avis
                     this.getGameDetails();
@@ -93,69 +125,85 @@
                     };
                 })
                 .catch((error) => {
-                    console.error("Erreur lors de la soumission de la revue", error);
+                    console.error(
+                        "Erreur lors de la soumission de la revue",
+                        error
+                    );
                 });
         },
-      deleteReview(reviewId) {
-        const gameId = this.$route.params.id;
-        axios.delete(`http://localhost:3000/games/review/${gameId}/${reviewId}`)
-            .then(() => {
-                this.getGameDetails();
-            })
-            .catch((error) => {
-                console.error("Erreur lors de la suppression de la revue", error);
-            });
-      },
-      goBack() {
-        this.$router.push({ path: '/' });
-      },
+        deleteReview(reviewId) {
+            const gameId = this.$route.params.id;
+            axios
+                .delete(
+                    `http://localhost:3000/games/review/${gameId}/${reviewId}`
+                )
+                .then(() => {
+                    this.getGameDetails();
+                })
+                .catch((error) => {
+                    console.error(
+                        "Erreur lors de la suppression de la revue",
+                        error
+                    );
+                });
+        },
+        goBack() {
+            this.$router.push({ path: "/" });
+        },
     },
-  };
-  </script>
-  
-<style scoped>
+};
+</script>
 
-p,h3,h2 {
+<style scoped>
+p,
+h3,
+.gamecard-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.gamecard {
+    max-width: 500px;
+}
+h2 {
     color: var(--text);
     font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
         sans-serif;
     text-align: justify;
-    padding: 20px ;
-    
+    padding: 20px;
 }
 .review-item {
-  list-style: none;
-  border: 1px solid #ddd; 
-  border-radius: 10px;
-  width: 30%;
-  margin-bottom: 15px;
-  background-color: rgb(255, 251, 251)000; 
+    list-style: none;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    width: 30%;
+    margin-bottom: 15px;
+    background-color: rgb(255, 251, 251) 000;
 }
 
 form {
-    position:absolute;
-    padding : 20px;
+    position: absolute;
+    padding: 20px;
     margin: 40px;
     left: 40%;
-    
+
     background-color: aliceblue;
     border-radius: 20px;
 }
-h2,h3{
-  text-align: center;
-  font-size: 2em;
+h2,
+h3 {
+    text-align: center;
+    font-size: 2em;
 }
 
-
 .form-group {
-  margin-bottom: 15px; 
+    margin-bottom: 15px;
 }
 
 label {
-  display: block; 
-  margin-bottom: 5px; 
+    display: block;
+    margin-bottom: 5px;
 }
-
-
-  </style>
-  
+</style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,109 +1,132 @@
-
 <template>
     <div class="home">
-      <SiteTopBar />
-  
-      <div class="list">
-        <h2>LIST OF GAMES</h2>
-        <router-link to="/addGame">
-          <button>Add Game</button>
-        </router-link>
-        <ul>
-          <li v-for="game in games" :key="game.game_id">
-            <router-link
-            :to="{ name: 'GameDetails', params: { id: game.game_id }}"
-            class="game-link">            
-            <GameCard
-              :title="game.game_name"
-              :category="game.category"
-              :release_date="game.release_date.substring(0, 10)"
-              :description="game.description"
-              :rating="game.age_rating"
-              :gameId="game.game_id"
-              @delete-game="deleteGame"
-              @edit-game="editGame"
-            />
-          </router-link>
-          </li>
-        </ul>
-      </div>
+        <SiteTopBar />
+        <div class="list">
+            <ul>
+                <li v-for="game in games" :key="game.game_id">
+                    <router-link
+                        :to="{
+                            name: 'GameDetails',
+                            params: { id: game.game_id },
+                        }"
+                        class="game-link"
+                    >
+                        <GameCard
+                            :title="game.game_name"
+                            :category="game.category"
+                            :release_date="game.release_date.substring(0, 10)"
+                            :description="game.description"
+                            :rating="game.age_rating"
+                            :gameId="game.game_id"
+                            @delete-game="deleteGame"
+                            @edit-game="editGame"
+                        />
+                    </router-link>
+                </li>
+            </ul>
+            <router-link to="/addGame">
+                <button class="add-button">ADD GAME</button>
+            </router-link>
+        </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  import SiteTopBar from "@/components/TopBarComponents/SiteTopBar.vue";
-  import GameCard from "@/components/SingleItemComponents/GameCard.vue";
-  
-  export default {
+</template>
+
+<script>
+import axios from "axios";
+import SiteTopBar from "@/components/TopBarComponents/SiteTopBar.vue";
+import GameCard from "@/components/SingleItemComponents/GameCard.vue";
+
+export default {
     name: "HomeComponent",
     components: {
-      SiteTopBar,
-      GameCard,
+        SiteTopBar,
+        GameCard,
     },
     data() {
-      return {
-        games: [],
-      };
+        return {
+            games: [],
+        };
     },
     mounted() {
-      this.recupGames();
+        this.recupGames();
     },
     methods: {
-      recupGames() {
-        axios
-          .get("http://localhost:3000/games")
-          .then((response) => {
-            this.games = response.data.games;
-          })
-          .catch((error) => {
-            console.error("Erreur lors de la récupération des jeux", error);
-          });
-      },
-      deleteGame(gameId) {
-        axios
-          .delete(`http://localhost:3000/games/delete/${gameId}`)
-          .then(() => {
-              console.log("Jeu supprimé avec succès");
-              this.recupGames();
-          })
-          .catch((error) => {
-              console.error("Erreur lors de la suppression du jeu", error);
-          });
+        recupGames() {
+            axios
+                .get("http://localhost:3000/games")
+                .then((response) => {
+                    this.games = response.data.games;
+                })
+                .catch((error) => {
+                    console.error(
+                        "Erreur lors de la récupération des jeux",
+                        error
+                    );
+                });
         },
-      editGame(gameId) {
-        console.log(gameId);
-        this.$router.push({ name: 'EditGame', params: { id: gameId } });
-      },
+        deleteGame(gameId) {
+            axios
+                .delete(`http://localhost:3000/games/delete/${gameId}`)
+                .then(() => {
+                    console.log("Jeu supprimé avec succès");
+                    this.recupGames();
+                })
+                .catch((error) => {
+                    console.error(
+                        "Erreur lors de la suppression du jeu",
+                        error
+                    );
+                });
+        },
+        editGame(gameId) {
+            console.log(gameId);
+            this.$router.push({ name: "EditGame", params: { id: gameId } });
+        },
     },
-  };
-  </script>
-  
-  <style scoped>
-  .list {
-    margin-top: 25px;
-    margin-left: 20px;
-    margin-right: 20px;
-  }
-  
-  h2 {
+};
+</script>
+
+<style scoped>
+.list {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+h2 {
     color: aliceblue;
-  }
-  
-  ul {
+}
+
+ul {
     list-style-type: none;
     padding: 0;
     display: flex;
     flex-wrap: wrap;
     gap: 20px;
-  }
-  
-  li {
-    width: calc(33.333% - 20px);
-  }
-  .game-link {
-  text-decoration: none;
-  color: inherit;
 }
-  </style>
-  
+
+li {
+    width: calc(33.333% - 20px);
+}
+.game-link {
+    text-decoration: none;
+    color: inherit;
+}
+
+.add-button {
+    background-color: white;
+    font: "Poppins", sans-serif;
+    border: 1px solid black;
+    box-shadow: 0 0 0 5px var(--primary);
+    font-weight: bold;
+    color: black;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+}
+</style>
